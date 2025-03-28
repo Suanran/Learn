b55846d7a60b3216b1c5df93c68528c1b2f9f87b
Suanran <895356017@qq.com>
2025-03-28 09:57:00 +0800

    创建next.js项目

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// pages/about.js
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is our company's about page.</p>
+            <p>Learn more about our mission, team, and values.</p>
+        </div>
+    );
+};
+
+export default About;
